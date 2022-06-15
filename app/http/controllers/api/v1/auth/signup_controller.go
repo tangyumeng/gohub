cb2077ae4030b89cb28ee0163dce7489d8b1a8e0
tangyumeng <tangyumeng@gmail.com>
2022-06-15 23:01:16 +0800

    JSON 解析和验证器封装

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -2,7 +2,6 @@
 package auth
 
 import (
-	"fmt"
 	v1 "gohub/app/http/controllers/api/v1"
 	"gohub/app/models/user"
 	"gohub/app/requests"
@@ -28,26 +27,30 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 	// }
 	// request := PhoneExistRequest{}
 
-	// 解析 JSON 请求
-	if err := c.ShouldBindJSON(&request); err != nil {
-		// 解析失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"error": err.Error(),
-		})
-		// 打印错误信息
-		fmt.Println(err.Error())
-		// 出错了，中断请求
-		return
-	}
+	// // 解析 JSON 请求
+	// if err := c.ShouldBindJSON(&request); err != nil {
+	// 	// 解析失败，返回 422 状态码和错误信息
+	// 	c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+	// 		"error": err.Error(),
+	// 	})
+	// 	// 打印错误信息
+	// 	fmt.Println(err.Error())
+	// 	// 出错了，中断请求
+	// 	return
+	// }
 
-	// 表单验证
-	errs := requests.ValidateSignupPhoneExist(&request, c)
-	// errs 返回长度等于零即通过，大于 0 即有错误发生
-	if len(errs) > 0 {
-		// 验证失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"errors": errs,
-		})
+	// // 表单验证
+	// errs := requests.ValidateSignupPhoneExist(&request, c)
+	// // errs 返回长度等于零即通过，大于 0 即有错误发生
+	// if len(errs) > 0 {
+	// 	// 验证失败，返回 422 状态码和错误信息
+	// 	c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+	// 		"errors": errs,
+	// 	})
+	// 	return
+	// }
+
+	if ok := requests.Validate(c, &request, requests.SignupPhoneExist); !ok {
 		return
 	}
 
@@ -63,26 +66,30 @@ func (sc *SignupController) IsEmailExist(c *gin.Context) {
 	// 初始化请求对象
 	request := requests.SignupEmailExistRequest{}
 
-	// 解析 JSON 请求
-	if err := c.ShouldBindJSON(&request); err != nil {
-		// 解析失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"error": err.Error(),
-		})
-		// 打印错误信息
-		fmt.Println(err.Error())
-		// 出错了，中断请求
-		return
-	}
+	// // 解析 JSON 请求
+	// if err := c.ShouldBindJSON(&request); err != nil {
+	// 	// 解析失败，返回 422 状态码和错误信息
+	// 	c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+	// 		"error": err.Error(),
+	// 	})
+	// 	// 打印错误信息
+	// 	fmt.Println(err.Error())
+	// 	// 出错了，中断请求
+	// 	return
+	// }
 
-	// 表单验证
-	errs := requests.ValidateSignupEmailExist(&request, c)
-	// errs 返回长度等于零即通过，大于 0 即有错误发生
-	if len(errs) > 0 {
-		// 验证失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"errors": errs,
-		})
+	// // 表单验证
+	// errs := requests.ValidateSignupEmailExist(&request, c)
+	// // errs 返回长度等于零即通过，大于 0 即有错误发生
+	// if len(errs) > 0 {
+	// 	// 验证失败，返回 422 状态码和错误信息
+	// 	c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+	// 		"errors": errs,
+	// 	})
+	// 	return
+	// }
+
+	if ok := requests.Validate(c, &request, requests.SignupEmailExist); !ok {
 		return
 	}
 
