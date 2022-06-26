b7ab75d3aa6ceea15d60af4a35da9b4cb3cb2953
tangyumeng <tangyumeng@qiyi.com>
2022-06-26 15:52:39 +0800

    图片验证码库(captcha)

---
+++ pkg/captcha/captcha.go
@@ -0,0 +1,56 @@
+package captcha
+
+import (
+	"gohub/pkg/app"
+	"gohub/pkg/config"
+	"gohub/pkg/redis"
+	"sync"
+
+	"github.com/mojocn/base64Captcha"
+)
+
+type Captcha struct {
+	Base64Captcha *base64Captcha.Captcha
+}
+
+var once sync.Once
+
+var internalCaptcha *Captcha
+
+func NewCaptcha() *Captcha {
+	once.Do(func() {
+		internalCaptcha = &Captcha{}
+
+		store := RedisStore{
+			RedisClient: redis.Redis,
+			KeyPrefix:   config.GetString("app.name") + ":captcha",
+		}
+
+		driver := base64Captcha.NewDriverDigit(
+			config.GetInt("captcha.height"),
+			config.GetInt("captcha.width"),
+			config.GetInt("captcha.length"),
+			config.GetFloat64("captcha.maxskew"),
+			config.GetInt("captcha.dotcount"),
+		)
+		internalCaptcha.Base64Captcha = base64Captcha.NewCaptcha(driver, &store)
+	})
+	return internalCaptcha
+}
+
+// GenerateCaptcha 生成图片验证码
+func (c *Captcha) GenerateCaptcha() (id string, b64s string, err error) {
+	return c.Base64Captcha.Generate()
+}
+
+// VerifyCaptcha 验证验证码是否正确
+func (c *Captcha) VerifyCaptcha(id string, answer string) (match bool) {
+
+	// 方便本地和 API 自动测试
+	if !app.IsProduction() && id == config.GetString("captcha.testing_key") {
+		return true
+	}
+	// 第三个参数是验证后是否删除，我们选择 false
+	// 这样方便用户多次提交，防止表单提交错误需要多次输入图片验证码
+	return c.Base64Captcha.Verify(id, answer, false)
+}
---
+++ pkg/captcha/store_redis.go
@@ -0,0 +1,42 @@
+package captcha
+
+import (
+	"errors"
+	"gohub/pkg/app"
+	"gohub/pkg/config"
+	"gohub/pkg/redis"
+	"time"
+)
+
+type RedisStore struct {
+	RedisClient *redis.RedisClient
+
+	KeyPrefix string
+}
+
+func (s *RedisStore) Set(key string, value string) error {
+	ExpireTime := time.Minute * time.Duration(config.GetInt64("chatcha.expire_time"))
+	// 方便本地开发调试
+	if app.IsLocal() {
+		ExpireTime = time.Minute * time.Duration(config.GetInt64("captcha.debug_expire_time"))
+	}
+
+	if ok := s.RedisClient.Set(s.KeyPrefix+key, value, ExpireTime); !ok {
+		return errors.New("无法存储图片验证码答案")
+	}
+	return nil
+}
+
+func (s *RedisStore) Get(key string, clear bool) string {
+	key = s.KeyPrefix + key
+	val := s.RedisClient.Get(key)
+	if clear {
+		s.RedisClient.Del(key)
+	}
+	return val
+}
+
+func (s *RedisStore) Verify(key, answer string, clear bool) bool {
+	v := s.Get(key, clear)
+	return v == answer
+}
